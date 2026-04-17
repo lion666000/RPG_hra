552ef726cc200163d196893faf6ecc45ed02a3f9
hlajak06 <hlajak06@gasos-ro.cz>
2026-04-17 08:28:30 +0200

    e

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Character {
 
     /* 🎓 ZÁKLAD 1: vytvoř atributy
@@ -6,11 +9,41 @@ public class Character {
      attack (int)
      inventory (ArrayList<String>) */
 
+    private String name;
+    private int hp;
+    private int attack;
+    private ArrayList<String> inventory;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void lowerHp(int hp) {
+        this.hp -= hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public ArrayList<String> getInventory() {
+        return inventory;
+    }
 
     // 🎓 ZÁKLAD 2: konstruktor
     // nastav jméno a vytvoř prázdný inventář
     public Character(String name) {
-
+        this.name = name;
+        inventory = new ArrayList<>();
     }
 
     /*
@@ -20,12 +53,14 @@ public class Character {
      * hint: zkus použít Random
      */
     public void generateStats() {
-
+        Random rand = new Random();
+        hp = rand.nextInt(50, 100);
+        attack =  rand.nextInt(5, 20);
     }
 
     // 🎓 ZÁKLAD 2: přidání itemu do inventory
     public void addItem(String item) {
-
+        inventory.add(item);
     }
 
     // ⭐ BONUS 2: odebrání itemu
@@ -37,6 +72,9 @@ public class Character {
     // 🎓 ZÁKLAD 2: výpis postavy - jmeno, hp, attack (bez itemů))
     public void printInfo() {
 
+        System.out.println("Name: " + name);
+        System.out.println("HP: " + hp);
+        System.out.println("Attack: " + attack);
         // ⭐ BONUS 1:
         // vypiš o inventář a to očíslovaně (1. item)
     }
--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,7 @@ public class Game {
         System.out.println("Program začal.");
         // 🎓 ZÁKLAD 6: načtení jména
         System.out.println("Zadej jméno:");
-        String name = "";
+        String name = sc.nextLine();
 
         // TODO načti jméno (patří do 🎓 ZÁKLAD 6)
 
@@ -17,17 +17,37 @@ public class Game {
           - odstraň mezery na začátku a konci (trim())
          - nastav první písmeno jako velké */
 
+        name = name.trim();
+
+        char fl = name.charAt(0);
+
+        String FL =  fl + "";
+
+        FL = FL.toUpperCase();
+
+        StringBuilder s =  new StringBuilder(name);
+
+        s.replace(0,1,FL);
+
+        name = s.toString();
+
         // TODO uprav name (patří do 🎓 ZÁKLAD 6)
 
 
         // 🎓 ZÁKLAD 8: vytvoř hráče (vytvoř novou instanci Character se jménem hráče)
-        Character player = null;
+        Character player = new Character(name);
+
 
         // 🎓 ZÁKLAD 9: vygeneruj staty hráče (generateStats())
+
+        player.generateStats();
 
         /* 🎓 ZÁKLAD 10: načti 3 itemy
          hint: zkus použít for cyklus */
 
+        for (int i = 0; i < 3; i++){
+            player.addItem("The sword of Tuah");
+        }
 
         int choice = 5;
         System.out.println();
@@ -44,15 +64,51 @@ public class Game {
 
             System.out.println("5 - konec");
             System.out.println("-------------------------------------------------------------------");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
             // TODO načti volbu (patří do 🎓 ZÁKLAD 11)
 
 
             if (choice == 1) {
+
+                player.printInfo();
+
                 // TODO vypiš postavu (patří do 🎓 ZÁKLAD 11)
             }
 
             if (choice == 2) {
+                System.out.println("Zadej název nepřítele:");
+                name = sc.nextLine();
+
+                name = name.trim();
+
+                fl = name.charAt(0);
+
+                FL =  fl + "";
+
+                FL = FL.toUpperCase();
+
+                s =  new StringBuilder(name);
+
+                s.replace(0,1,FL);
+
+                name = s.toString();
+
+                Character opponent = new Character(name);
+
+                opponent.generateStats();
+                for (int i = 0; i < 3; i++){
+                    opponent.addItem("The mace of Rah");
+                }
+
                 // TODO vytvoř nepřítele (Character) (patří do 🎓 ZÁKLAD 11)
+
+                opponent.printInfo();
+
+                fight(player, opponent);
+
                 // TODO zavolej fight() (patří do 🎓 ZÁKLAD 11)
             }
 
@@ -94,13 +150,42 @@ public class Game {
      */
     public void fight(Character a, Character b) {
 
-        System.out.println("Souboj začíná!");
+        System.out.println("Souboj začíná!\n");
 
         /* hint: zkus použít while cyklus
          - cyklus běží dokud mají oba HP > 0
          - a útočí na b  ( patří do 🎓 ZÁKLAD 12)*/
 
         // ⭐ BONUS 4: kritický zásah =  s pravděpodobností 20 % způsobí 3x vyšší poškození
+
+        boolean combat = true;
+
+        while (combat){
+            int damageDealt = 0;
+
+            if (a.getHp() > 0 &&  b.getHp() > 0) {
+                damageDealt = a.getAttack();
+                b.lowerHp(damageDealt);
+
+                System.out.println(a.getName() + " zaůtočil na " + b.getName() + " a ubral " + damageDealt + " HP, " + b.getName() + " nyní má " + b.getHp() + " HP");
+
+                if (b.getHp() > 0){
+                    damageDealt = b.getAttack();
+                    a.lowerHp(damageDealt);
+
+                    System.out.println(b.getName() + " zaůtočil na " + a.getName() + " a ubral " + damageDealt + " HP, " + a.getName() + " nyní má " + a.getHp() + " HP");
+                }
+            }
+            else {
+                if (a.getHp() <= 0) {
+                    System.out.println("\n" + a.getName() + " vyhrál");
+                }
+                else {
+                    System.out.println("\n" + b.getName() + " vyhrál");
+                }
+                combat = false;
+            }
+        }
 
         // TODO kontrola jestli b žije (patří do 🎓 ZÁKLAD 12)
 
